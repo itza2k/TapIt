5f5c0d201503fca960307a99f2c959fee1b108b8
itza2k <161753369+itza2k@users.noreply.github.com>
2025-02-21 23:50:35 +0530

    added libs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,4 +56,8 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    implementation (libs.kotlinx.serialization.json)
+    implementation (libs.kotlinx.coroutines.android)
+    implementation (libs.androidx.material.icons.extended)
+
 }
