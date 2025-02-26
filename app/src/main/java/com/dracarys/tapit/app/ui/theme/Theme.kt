ba99951e33bf11bd7eb78f29803a0053066bb8e1
itza2k <161753369+itza2k@users.noreply.github.com>
2025-02-26 20:18:25 +0530

    material theme

--- app/src/main/java/com/dracarys/tapit/app/ui/theme/Theme.kt
+++ app/src/main/java/com/dracarys/tapit/app/ui/theme/Theme.kt
@@ -3,56 +3,83 @@ package com.dracarys.tapit.app.ui.theme
 import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
-import androidx.compose.material3.lightColorScheme
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = Color(0xFF86CEFF),
+    onPrimary = Color(0xFF00344F),
+    primaryContainer = Color(0xFF004B70),
+    onPrimaryContainer = Color(0xFFCCE5FF),
+    secondary = Color(0xFFB4CAD6),
+    onSecondary = Color(0xFF1F333C),
+    secondaryContainer = Color(0xFF354A53),
+    onSecondaryContainer = Color(0xFFD0E6F2),
+    tertiary = Color(0xFFC9C3EA),
+    onTertiary = Color(0xFF302D4C),
+    tertiaryContainer = Color(0xFF464364),
+    onTertiaryContainer = Color(0xFFE5DEFF),
+    background = Color(0xFF191C1E),
+    onBackground = Color(0xFFE1E2E5),
+    surface = Color(0xFF191C1E),
+    onSurface = Color(0xFFE1E2E5),
+    surfaceVariant = Color(0xFF40484C),
+    onSurfaceVariant = Color(0xFFC0C7CD)
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    primary = Color(0xFF006590),
+    onPrimary = Color(0xFFFFFFFF),
+    primaryContainer = Color(0xFFCCE5FF),
+    onPrimaryContainer = Color(0xFF001E30),
+    secondary = Color(0xFF4D616C),
+    onSecondary = Color(0xFFFFFFFF),
+    secondaryContainer = Color(0xFFD0E6F2),
+    onSecondaryContainer = Color(0xFF081E27),
+    tertiary = Color(0xFF5D5B7D),
+    onTertiary = Color(0xFFFFFFFF),
+    tertiaryContainer = Color(0xFFE5DEFF),
+    onTertiaryContainer = Color(0xFF1A1836),
+    background = Color(0xFFFBFCFF),
+    onBackground = Color(0xFF191C1E),
+    surface = Color(0xFFFBFCFF),
+    onSurface = Color(0xFF191C1E)
 )
 
 @Composable
 fun TapItTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
         dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
             val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+            if (darkTheme) dynamicDarkColorScheme(context)
+            else dynamicLightColorScheme(context)
         }
-
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
+    }
+
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            window.statusBarColor = colorScheme.background.toArgb()
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
+        }
     }
 
     MaterialTheme(
         colorScheme = colorScheme,
         typography = Typography,
+        shapes = Shapes,
         content = content
     )
 }
